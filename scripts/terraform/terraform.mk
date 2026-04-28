23b4155d93ecfda9f089a27cd074c8c24336773a
Alastair Lock <alastair.lock@gmail.com>
2026-04-28 13:17:02 +0100

    wip

--- scripts/terraform/terraform.mk
+++ scripts/terraform/terraform.mk
@@ -30,7 +30,7 @@ prod: # Target the prod environment - make prod <action>
 review: # Target the review infrastructure, or a review app if PR_NUMBER is used - make review <action> [PR_NUMBER=<pr_number>]
 	$(eval include infrastructure/environments/review/variables.sh)
 	$(if ${PR_NUMBER}, $(eval export TF_VAR_deploy_infra=false), $(eval export TF_VAR_deploy_container_apps=true))
-	$(if ${!PR_NUMBER},$(eval export TF_VAR_deploy_container_apps=false),)
+	$(if ${PR_NUMBER},,$(eval export TF_VAR_deploy_container_apps=false))
 	$(if ${PR_NUMBER}, $(eval export ENVIRONMENT=pr-${PR_NUMBER}), $(eval export ENVIRONMENT=review))
 
 db-setup:
